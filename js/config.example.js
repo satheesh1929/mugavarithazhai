d46115bb778d94fdfc0165bddc74cac20d615d94
satheesh1929 <satheeshkumar.tech29@gmail.com>
2026-03-20 09:59:47 +0530

    neww

---
+++ js/config.example.js
@@ -0,0 +1,10 @@
+/**
+ * CONFIGURATION TEMPLATE
+ * Copy this to js/config.js and add your real keys for local development.
+ * This file is ignored by Git to keep your keys secure.
+ */
+
+const CONFIG = {
+    RAZORPAY_KEY_ID: 'rzp_live_YOUR_KEY_ID',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec'
+};
